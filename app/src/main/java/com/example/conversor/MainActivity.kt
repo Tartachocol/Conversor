213fbd288b20ae62a7fa27e2c65a81e481140302
Tartachocol <flcara278@ieszaidinvergeles.org>
2022-12-21 20:25:07 +0100

    Funciona el conversor

--- app/src/main/java/com/example/conversor/MainActivity.kt
+++ app/src/main/java/com/example/conversor/MainActivity.kt
@@ -6,10 +6,18 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
+    /*
+        Millas  a   KM
+        Litros a Galones
+        Kilos a Libras
+        Dolares a Euros
+        Metros  a   Pies
+     */
     private var resultado: String = ""
+    private lateinit var et2: TextView
     private lateinit var tv1: TextView
     private lateinit var tv2: TextView
+
     private var c: conversor = conversor()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +26,14 @@ class MainActivity : AppCompatActivity() {
         var button = findViewById<Button>(R.id.button)
         tv1 = findViewById<TextView>(R.id.textView1)
         tv2 = findViewById<TextView>(R.id.textView2)
-        var et1 = findViewById<TextView>(R.id.editText1)
-        var et2 = findViewById<TextView>(R.id.editText2)
-        //Array Con opcines
-        val listOptions = arrayOf("Millas a KM", "KM a Millas")
+        var et1 = findViewById<EditText>(R.id.editText1)
+        et2 = findViewById<TextView>(R.id.editText2)
+
+        //Array con las opcines del spinner
+        val listOptions = arrayOf(
+            "Millas a KM", "KM a Millas", "Litros a Galones", "Galones a Litros", "Kilos a Libras",
+            "Libras a Kilos", "Dolares a Euros", "Euros a Dolares", "Metros a Pies", "Pies a Metros"
+        )
         val spinner = findViewById<Spinner>(R.id.spinner_list)
 
         //Mete las opcines en el espiner
@@ -39,7 +51,9 @@ class MainActivity : AppCompatActivity() {
 
             /**
              *   Detecta si has seleccionado lo muestra en el spinner y
-             *   ademas detecta la posición que se a seleccionado
+             *   ademas detecta la posición que se a seleccionado.
+             *
+             *   Al usar esta funcion hace que la conversion se haga automaticamente al cambiar de unidades
              */
 
             override fun onItemSelected(
@@ -49,33 +63,71 @@ class MainActivity : AppCompatActivity() {
                 id: Long
             ) {
                 Toast.makeText(this@MainActivity, listOptions[position], Toast.LENGTH_SHORT).show()
+
+                //La opcion por defecto es la primera opcion
                 when (position) {
 
-                    1 -> setTexto("KM", "Millas")
-                    else -> setTexto("Millas", "KM")
+                    1 -> setTexto("KM", "Millas", 1, et1.text.toString().toDouble())
+                    2 -> setTexto("Litros", "Galones", 2, et1.text.toString().toDouble())
+                    3 -> setTexto("Galones", "Litros", 3, et1.text.toString().toDouble())
+                    4 -> setTexto("Kilos", "Libras", 4, et1.text.toString().toDouble())
+                    5 -> setTexto("Libras", "Kilos", 5, et1.text.toString().toDouble())
+                    6 -> setTexto("Dolares", "Euros", 6, et1.text.toString().toDouble())
+                    7 -> setTexto("Euros", "Dolares", 7, et1.text.toString().toDouble())
+                    8 -> setTexto("Metros", "Pies", 8, et1.text.toString().toDouble())
+                    9 -> setTexto("Pies", "Metros", 9, et1.text.toString().toDouble())
+                    else -> setTexto("Millas", "KM", 0, et1.text.toString().toDouble())
                 }
-
-
-
-
 
                 button.setOnClickListener {
-                    when (spinner.selectedItem.toString()) {
-                        "Millas a KM" -> setTexto("Millas", "KM")
-                        "KM a Millas" -> tv1.text = "KM"
+                    when (position) {
+
+                        1 -> setTexto("KM", "Millas", 1, et1.text.toString().toDouble())
+                        2 -> setTexto("Litros", "Galones", 2, et1.text.toString().toDouble())
+                        3 -> setTexto("Galones", "Litros", 3, et1.text.toString().toDouble())
+                        4 -> setTexto("Kilos", "Libras", 4, et1.text.toString().toDouble())
+                        5 -> setTexto("Libras", "Kilos", 5, et1.text.toString().toDouble())
+                        6 -> setTexto("Dolares", "Euros", 6, et1.text.toString().toDouble())
+                        7 -> setTexto("Euros", "Dolares", 7, et1.text.toString().toDouble())
+                        8 -> setTexto("Metros", "Pies", 8, et1.text.toString().toDouble())
+                        9 -> setTexto("Pies", "Metros", 9, et1.text.toString().toDouble())
+                        else -> setTexto("Millas", "KM", 0, et1.text.toString().toDouble())
                     }
-
                 }
-
             }
+
+
         }
 
 
     }
 
-    fun setTexto(texto1: String, texto2: String): Unit {
+    //La posicion la usa para saber a que  if entrar y hacer la funcion correspondiente
+    fun setTexto(texto1: String, texto2: String, posicion: Int, numero: Double): Unit {
         tv1.text = texto1
         tv2.text = texto2
+        if (posicion == 0) {
+            et2.text = c.MillasAKilometros(numero).toString()
+
+        } else if (posicion == 1) {
+            et2.text = c.KilometrosAMillas(numero).toString()
+        } else if (posicion == 2) {
+            et2.text = c.litrosAGalones(numero).toString()
+        } else if (posicion == 3) {
+            et2.text = c.GalonesALitros(numero).toString()
+        } else if (posicion == 4) {
+            et2.text = c.KilosALibras(numero).toString()
+        } else if (posicion == 5) {
+            et2.text = c.LibrasAKilos(numero).toString()
+        } else if (posicion == 6) {
+            et2.text = c.DolaresAEuros(numero).toString()
+        } else if (posicion == 7) {
+            et2.text = c.EurosADolares(numero).toString()
+        } else if (posicion == 8) {
+            et2.text = c.MetrosAPieas(numero).toString()
+        } else if (posicion == 9) {
+            et2.text = c.PiesAMetros(numero).toString()
+        }
 
 
     }
