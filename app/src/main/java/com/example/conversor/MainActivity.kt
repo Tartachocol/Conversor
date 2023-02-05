5138ae344225459bedb1be361458da5d5cd98471
Tartachocol <flcara278@ieszaidinvergeles.org>
2023-02-05 19:55:39 +0100

    Terminado

--- app/src/main/java/com/example/conversor/MainActivity.kt
+++ app/src/main/java/com/example/conversor/MainActivity.kt
@@ -6,13 +6,7 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    /*
-        Millas  a   KM
-        Litros a Galones
-        Kilos a Libras
-        Dolares a Euros
-        Metros  a   Pies
-     */
+
     private var resultado: String = ""
     private lateinit var et2: TextView
     private lateinit var tv1: TextView
@@ -24,17 +18,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var button = findViewById<Button>(R.id.button)
-        tv1 = findViewById<TextView>(R.id.textView1)
-        tv2 = findViewById<TextView>(R.id.textView2)
-        var et1 = findViewById<EditText>(R.id.editText1)
-        et2 = findViewById<TextView>(R.id.editText2)
+        tv1 = findViewById<TextView>(R.id.tvUd1)
+        tv2 = findViewById<TextView>(R.id.tvUd2)
+        var et1 = findViewById<EditText>(R.id.edtNumeroAConvertir)
+        et2 = findViewById<TextView>(R.id.tvNumConvertido)
 
         //Array con las opcines del spinner
         val listOptions = arrayOf(
             "Millas a KM", "KM a Millas", "Litros a Galones", "Galones a Litros", "Kilos a Libras",
             "Libras a Kilos", "Dolares a Euros", "Euros a Dolares", "Metros a Pies", "Pies a Metros"
         )
-        val spinner = findViewById<Spinner>(R.id.spinner_list)
+        val spinner = findViewById<Spinner>(R.id.spinnerList)
 
         //Mete las opcines en el espiner
         val arrayAdapter =
@@ -82,16 +76,16 @@ class MainActivity : AppCompatActivity() {
                 button.setOnClickListener {
                     when (position) {
 
-                        1 -> setTexto("KM", "Millas", 1, et1.text.toString().toDouble())
-                        2 -> setTexto("Litros", "Galones", 2, et1.text.toString().toDouble())
-                        3 -> setTexto("Galones", "Litros", 3, et1.text.toString().toDouble())
-                        4 -> setTexto("Kilos", "Libras", 4, et1.text.toString().toDouble())
-                        5 -> setTexto("Libras", "Kilos", 5, et1.text.toString().toDouble())
-                        6 -> setTexto("Dolares", "Euros", 6, et1.text.toString().toDouble())
-                        7 -> setTexto("Euros", "Dolares", 7, et1.text.toString().toDouble())
-                        8 -> setTexto("Metros", "Pies", 8, et1.text.toString().toDouble())
-                        9 -> setTexto("Pies", "Metros", 9, et1.text.toString().toDouble())
-                        else -> setTexto("Millas", "KM", 0, et1.text.toString().toDouble())
+                        1 -> setTexto("KM", "Millas", position, et1.text.toString().toDouble())
+                        2 -> setTexto("Litros", "Galones", position, et1.text.toString().toDouble())
+                        3 -> setTexto("Galones", "Litros", position, et1.text.toString().toDouble())
+                        4 -> setTexto("Kilos", "Libras", position, et1.text.toString().toDouble())
+                        5 -> setTexto("Libras", "Kilos", position, et1.text.toString().toDouble())
+                        6 -> setTexto("Dolares", "Euros", position, et1.text.toString().toDouble())
+                        7 -> setTexto("Euros", "Dolares", position, et1.text.toString().toDouble())
+                        8 -> setTexto("Metros", "Pies", position, et1.text.toString().toDouble())
+                        9 -> setTexto("Pies", "Metros", position, et1.text.toString().toDouble())
+                        else -> setTexto("Millas", "KM", position, et1.text.toString().toDouble())
                     }
                 }
             }
--- app/src/main/java/com/example/conversor/conversor.kt
+++ app/src/main/java/com/example/conversor/conversor.kt
@@ -1,65 +1,69 @@
 package com.example.conversor
 
 class conversor() {
+    /**
+     * Millas   a   KM
+     * Litros   a   Galones
+     * Kilos    a   Libras
+     * Dolares  a   Euros
+     * Metros   a   Pies
+     */
 
+
+    val km: Double = 0.62137
+    val litros: Double = 3.785411784
+    val kl: Double = 2.2046
+    val euros: Double = 1.02335
+    val metro: Double = 3.2808
+    var numero: Double = 0.0
 
     fun KilometrosAMillas(num: Double): Double {
-        var num = num
-        num *= 0.62137
-        return num
+        numero = num * km
+        return numero
     }
 
     fun MillasAKilometros(num: Double): Double {
-        var num = num
-        num /= 0.62137
-        return num
+        numero = num / km
+        return numero
     }
 
     fun litrosAGalones(num: Double): Double {
-        var num = num
-        num /= 3.785411784
-        return num
+        numero = num / litros
+        return numero
     }
 
     fun GalonesALitros(num: Double): Double {
-        var num = num
-        num *= 3.785411784
-        return num
+        numero = num * litros
+        return numero
     }
 
     fun KilosALibras(num: Double): Double {
-        var num = num
-        num *= 2.2046
-        return num
+        numero = num * kl
+        return numero
     }
 
     fun LibrasAKilos(num: Double): Double {
-        var num = num
-        num /= 2.2046
-        return num
+        numero = num / kl
+        return numero
     }
 
     fun EurosADolares(num: Double): Double {
-        var num = num
-        num /= 1.02335
-        return num
+        numero = num / euros
+        return numero
     }
 
     fun DolaresAEuros(num: Double): Double {
-        var num = num
-        num *= 1.02335
-        return num
+        numero = num * euros
+        return numero
     }
 
     fun MetrosAPieas(num: Double): Double {
-        var num = num
-        num *= 3.2808
-        return num
+        numero = num * metro
+        return numero
     }
 
     fun PiesAMetros(num: Double): Double {
-        var num = num
-        num /= 3.2808
-        return num
+        numero = num / metro
+        return numero
     }
 }
